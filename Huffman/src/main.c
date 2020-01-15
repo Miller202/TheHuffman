0c4c5b9a0bd14827ac751a2793613e7004524604
Márcio Henrique <ikke.guerreiro@gmail.com>
2020-01-15 01:33:46 -0300

    Arquivo para testes

--- Huffman/src/main.c
+++ Huffman/src/main.c
@@ -5,8 +5,11 @@
 #include "../inc/heap.h"
 #include "../inc/hash.h"
 #include "../inc/huff_tree.h"
+#include "../inc/compress.h"
+#include "../inc/decompress.h"
 
 int main()
 {
-
+    open_files("..\\Testes\\teste.txt");
+    return 0;
 }
