cb19936e4ad4cc7f36c9f0eedf493d54b16ea8c1
michelthomas <michel.lins17@gmail.com>
2020-01-16 04:44:03 -0300

    Criação de utils.c;

---
+++ Huffman/lib/inc/utils.h
@@ -0,0 +1,24 @@
+#ifndef HUFFMAN_UTILS_H
+#define HUFFMAN_UTILS_H
+
+#include <stdio.h>
+#include "heap.h"
+#include "hash.h"
+#include "huff_tree.h"
+
+/**
+* 	Criar uma fila de prioridade(heap), a partir da frequência dos caracteres do arquivo
+* 	@param: recebe um array com a frequencia dos caracteres e um heap
+*	@return: cria nós com caracteres de qualquer frequencia e os enfileira no heap
+**/
+void create_priority_queue(long long int *frequency, HEAP *heap);
+
+/**
+* 	@param: recebe o ponteiro do arquivo de entrada
+*	@return: retorna um array com as frequências dos caracteres do arquivo
+**/
+long long int *frequency_counter(FILE *file);
+
+HEAP *mount_heap(FILE *file);
+
+#endif //HUFFMAN_UTILS_H
